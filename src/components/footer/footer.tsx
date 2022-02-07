244ba0e0e1e55f5dfba5362291c7293b66ac2139
Hamid Berkaine <hamiddev78@gmail.com>
2022-02-07 15:10:02 +0100

    add navbar in footer file component

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './../../sass/footer.scss'
 
 function Footer() {
-  return <div>Footer</div>;
+  return <footer className='footerContainer'>
+      <p className="credit">© By RJ and Hamid</p>
+    <nav className="navFooterContainer">
+      <ul className='ulNav'>
+        <li><Link to="/mention">Mentions</Link></li>
+        <li><Link to="/confidencial">confidential</Link></li>
+        <li><Link to="/road-map">Road Map</Link></li>
+      </ul>
+    </nav>
+  </footer>;
 }
 
 export default Footer;
